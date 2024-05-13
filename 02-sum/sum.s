033a3903f3b0f2c8db05d13ca7d363052a67f326
Tom <hernandeztomas584@gmail.com>
2024-05-13 15:27:58 -0300

    hola

--- 02-sum/sum.s
+++ 02-sum/sum.s
@@ -1,14 +1,12 @@
-
 .global _start
 
 _start: 
-    li      a0, 1           # Carga el primer número (1) en a0
-    li      a1, 2           # Carga el segundo número (2) en a1
-    add     a0, a0, a1      # Suma los dos números
+    li      a0, 1           
+    li      a1, 2           
+    add     a0, a0, a1      
 
-    lw      a1, 4(sp)       # Recupera el registro de retorno de la pila
-    lw      ra, 0(sp)       # Restaura el registro de retorno
-    addi    sp, sp, 8       # Libera el espacio reservado en la pila
-    jr      ra              # Retorna a la dirección de retorno
+    lw      a1, 4(sp)       
+    lw      ra, 0(sp)       
+    addi    sp, sp, 8      
+    jr      ra             
 
-# Fin del programa

74560327c1da52daa5c4e455a46260fcc0c51984
Tom <hernandeztomas584@gmail.com>
2024-06-08 19:50:57 -0300

    add: sumatoria usando sumatoria de gauss

--- 09-sumatoria/sumatoria.s
+++ 09-sumatoria/sumatoria.s
@@ -17,3 +17,14 @@
 #Primera vez
 # loop: 1 > 1? No, entonces acum = 1, incrementador = 2
 # loop: 2 > 1? Sí, entonces salgo.  
+
+#Utilizando Gauss n(n+1) / 2
+.data 
+    n: .word 1
+.text
+    lw s1, n #n
+    li t1, 2 #para dividir 
+    addi t2, s1, 1 # el n+1
+    mul s1, s1, t2 # n = n(n+1)
+    div s1, s1, t1 # n/2
+    
